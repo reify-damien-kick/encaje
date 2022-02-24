6d5ced4f404bda798f7da5d5a3a4aaeb1af29d29
Damien Kick <dkick1@mac.com>
2022-02-24 16:41:42 -0600

    lein new encaje

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to encaje
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/encaje/core_test.clj
@@ -0,0 +1,7 @@
+(ns encaje.core-test
+  (:require [clojure.test :refer :all]
+            [encaje.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
