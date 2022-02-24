54365662c3f31a4278a92cdac7445741c8565c9d
Damien Kick <dkick1@mac.com>
2022-02-24 16:57:51 -0600

    I made a pun!

--- src/encaje/core.clj
+++ src/encaje/core.clj
@@ -2,5 +2,6 @@
 
 (defmacro -- [& body]
   "Just a utility to give indentation: a threading macro which does
-  nothing but allows for some better indentation (IMNSHO)"
+  nothing but allows for some better indentation (IMNSHO). Minus minus
+  just substracts itself!"
   `(~@body))
