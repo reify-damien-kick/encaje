1032104b8cdfcfc4b17046d7840b91f020803cca
Damien Kick <dkick1@mac.com>
2022-02-25 11:27:37 -0600

    Fix up comments

--- src/encaje/core.clj
+++ src/encaje/core.clj
@@ -1,7 +1,6 @@
 (ns encaje.core)
 
 (defmacro -- [& body]
-  "Just a utility to give indentation: a threading macro which does
-  nothing but allows for some better indentation (IMNSHO). Minus minus
-  just substracts itself!"
+  "A threading macro which does nothing but allows for some better
+  indentation (IMNSHO). Minus minus just substracts itself!"
   `(~@body))
