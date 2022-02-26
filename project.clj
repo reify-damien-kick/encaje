bdae13dbefffd086a9e717fc852792664d559566
Damien Kick <dkick1@mac.com>
2022-02-26 15:57:24 -0600

    Get ready to add this to Clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject encaje "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+(defproject org.clojars.dkick/encaje "0.1.0"
+  :description "A collection of Clojure utilities: Clojure Enca(s|j)e(d)"
+  :url "https://github.com/dkick"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :repl-options {:init-ns encaje.core})
