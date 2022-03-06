e26c274137508be66000384e70b97e531d66aea6
Damien Kick <dkick1@mac.com>
2022-03-06 08:34:38 -0600

    Add the Escrow type and the -|| (book'ing) macro

--- src/encaje/core.clj
+++ src/encaje/core.clj
@@ -1,6 +1,22 @@
-(ns encaje.core)
+(ns encaje.core
+  (:import [java.io Closeable]))
 
 (defmacro -- [& body]
   "A threading macro which does nothing but allows for some better
   indentation (IMNSHO). Minus minus just substracts itself!"
   `(~@body))
+
+(defrecord Escrow [asset on-close]
+  clojure.lang.IDeref
+  (deref [escrow]
+    (.asset escrow))
+
+  Closeable
+  (close [escrow]
+    ((.on-close escrow) (.asset escrow))))
+
+(defmacro -|| [on-close & body]
+  `(->Escrow ~body ~(if (seq? on-close)
+                      `~on-close
+                      `(fn [asset#]
+                         (~on-close asset#)))))
