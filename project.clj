64f4b8155f6d517d3d9bc1a06c334f7fc8d76d27
Damien Kick <damien.kick@reifyhealth.com>
2022-06-28 13:03:20 -0500

    Fix misplaced doc string

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/dkick"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.0"]]
+  :plugins [[lein-ancient "1.0.0-RC3"]]
   :repl-options {:init-ns encaje.core}
   :signing {:gpg-key "06C9F682577E497967DF60D4CF53CBC793B99019"})
--- src/encaje/core.clj
+++ src/encaje/core.clj
@@ -1,12 +1,16 @@
 (ns encaje.core
   (:import [java.io Closeable]))
 
-(defmacro -- [& body]
+(defmacro --
   "A threading macro which does nothing but allows for some better
   indentation (IMNSHO). Minus minus just substracts itself!"
+  [& body]
   `(~@body))
 
 (defrecord Escrow [asset on-close]
+  ;; escrow: a bond, deed, or other document kept in the custody of a
+  ;; third party and taking effect only when a specified condition has
+  ;; been fulfilled.
   clojure.lang.IDeref
   (deref [escrow]
     (.asset escrow))
