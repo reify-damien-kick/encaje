dbce0db312e8a33c15d490f1814d1d164c716e1b
Damien Kick <damien.kick@reifyhealth.com>
2022-07-25 11:02:55 -0500

    Add a prefer-method for Escrow. Add more doc strings.

--- src/encaje/core.clj
+++ src/encaje/core.clj
@@ -1,12 +1,14 @@
 (ns encaje.core
+  (:require [clojure.pprint])
   (:import [java.io Closeable]))
 
 (defmacro --
   "A threading macro which does nothing but allows for some better
-  indentation (IMNSHO). Minus minus just substracts itself! I also
-  like that it looks like an SQL comment: this is code that is making
-  a comment about the indentation that is desired. I also like that it
-  looks like a dash that one might use in a list of things:
+  indentation (IMNSHO). Minus minus just substracts itself! Basically,
+  this is a macro version of identity. I also like that it looks like
+  an SQL comment: this is code that is making a comment about the
+  indentation that I want. I also like that it looks like a dash that
+  one might use in a list of things:
 
   Groceries:
   -- milk
@@ -19,7 +21,21 @@
 (defmacro fx
   "apply done as a macro. fn defines a function. fx is an application of
   a function to x: (fx + 1 2) is (+ 1 2) but without creating or
-  needing a seq for the args, x instead of xs"
+  needing a seq for the args, x instead of xs. Another opinionated
+  take on how one can make the code ident like I want it to indent,
+  similar to -- but different:
+
+  (def a-really-long-and-annoying-name-for-addtion +)
+
+  (a-really-long-and-annoying-name-for-addtion 1
+                                               2)
+
+  (a-really-long-and-annoying-name-for-addtion
+   1 2)
+
+  (fx a-really-long-and-annoying-name-for-addtion
+      1 2)
+  "
   [f & x-body]
   `(~f ~@x-body))
 
@@ -29,6 +45,10 @@
   the macro fx."
   [f & xs]
   (apply f xs))
+
+(prefer-method clojure.pprint/simple-dispatch
+               clojure.lang.IPersistentMap
+               clojure.lang.IDeref)
 
 (defrecord Escrow [asset on-close]
   ;; escrow: a bond, deed, or other document kept in the custody of a
@@ -43,6 +63,7 @@
     ((.on-close escrow) (.asset escrow))))
 
 (defmacro -|| [on-close & body]
+  ;; -|| is supposed to look like an account book
   `(->Escrow ~body ~(if (seq? on-close)
                       `~on-close
                       `(fn [asset#]
